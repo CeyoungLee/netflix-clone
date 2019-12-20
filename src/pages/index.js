71cfa6c4bba8ff212d106817578843238a8e72a6
CeyoungLee <cey.o.lee@gmail.com>
2019-12-20 16:43:23 +0900

    installed router-dom and init

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+class Main extends Component {
+  render() {
+    return (
+        <div>
+          <h1>The Main Component</h1>
+        </div>
+    )
+  }
+}
+
+export default Main;
